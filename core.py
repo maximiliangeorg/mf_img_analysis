965896e1d08ed0288a84836e1b01324aa0e6cac4
maximiliangeorg <maximiliangeorg@yahoo.de>
2016-12-07 15:43:51 -0300

    initial commit

---
+++ core.py
@@ -0,0 +1,330 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import mahotas as mh
+import matplotlib.pyplot as plt
+import math
+import sys
+from operator import itemgetter
+from matplotlib.backends.backend_pdf import PdfPages
+from os import listdir
+
+default = True
+validanswer = False
+while not validanswer:
+    userin = raw_input("do you want to use the user_input.py file? (y/n)")
+    if userin == "y":
+        from user_input import *
+        print "continue with user input"
+        validanswer = True
+        default = False
+    elif userin == "n":
+        print "continue with default settings"
+        validanswer = True
+    else:
+        print "invalid input, type 'y' or 'n'"
+        validanswer = False
+
+
+plt.gcf().clear()
+
+
+def listdir_nohidden(path):
+    newlist = []
+    for f in listdir(path):
+        if not f.startswith('.'):
+             newlist.append(f)
+    return newlist
+days = dict()
+inputimages = dict()
+imcount = 0
+path = "input/"
+daystemp = listdir_nohidden(path)
+
+for x in range(len(daystemp)):
+    days[x] = dict()
+    days[x][0] = str(daystemp[x])
+    path = "input/" + str(days[x][0]) + "/"
+    concentrationstemp = listdir_nohidden(str(path))
+    for n in range(len(concentrationstemp)):
+        days[x][n+1] = dict()
+        days[x][n+1][0] = str(concentrationstemp[n])
+        path = "input/" + str(days[x][0]) + "/" + str(days[x][n+1][0]) + "/"
+        imagestemp = listdir_nohidden(str(path))
+        for i in range(len(imagestemp)):
+            path = "input/" + str(days[x][0]) + "/" + str(days[x][n+1][0]) + "/" + str(imagestemp[i])
+            print "loaded: " + str(path)
+            days[x][n+1][i+1] = mh.imread(str(path))
+            days[x][n+1][i+1] = mh.colors.rgb2gray(days[x][n+1][i+1])
+            days[x][n+1][i+1] = days[x][n+1][i+1].astype(np.uint8)
+            inputimages[imcount] = dict()
+            # index 0 is image
+            inputimages[imcount][0] = days[x][n+1][i+1]
+            # index 1 is day
+            inputimages[imcount][1] = days[x][0]
+            # index 2 is concentration
+            inputimages[imcount][2] = days[x][n+1][0]
+            # index 3 is cell area
+            inputimages[imcount][3] = 0
+            imcount += 1
+
+pp = PdfPages('protocol.pdf')
+
+
+def cut(image, cuts):
+    xcord = image.shape[1]
+    ycord = image.shape[0]
+    xcord = float(xcord)
+    ycord = float(ycord)
+    p = 0
+    q = 0.0
+    k = 0.0
+    cuts = float(cuts)
+    imdict = dict()
+    while p < (cuts ** 2):
+        yslicestart = int((k/cuts) * ycord)
+        ysliceend = int(((k+1)/cuts) * ycord)
+        xslicestart = int((q/cuts) * xcord)
+        xsliceend = int(((q+1)/cuts) * xcord)
+        imageslice = image[yslicestart:ysliceend, xslicestart:xsliceend]
+        imdict[int(p)] = imageslice
+        q += 1
+        if q == cuts:
+            k += 1
+            q = 0.0
+        p += 1
+    return imdict
+
+
+def adjustsize(origimage, slicedimage, cuts):
+    if origimage.shape[0] % cuts != 0 or origimage.shape[1] % cuts != 0:
+        allx = []
+        ally = []
+        for im in range(len(slicedimage)):
+            allx.append(slicedimage[im].shape[1])
+            ally.append(slicedimage[im].shape[0])
+        allx.sort()
+        ally.sort()
+        xmin = allx[0]
+        ymin = ally[0]
+        for im in range(len(slicedimage)):
+            slicedimage[im] = slicedimage[im][0:ymin, 0:xmin]
+
+
+def getmins(image):
+    minvalues = []
+    for h in range(len(image)):
+        minvalues.append(image[h].min())
+    minvalues.sort()
+    return minvalues
+
+
+def getlimit(image):
+    minvalues = getmins(image)
+    gaps = []
+    avmin = 0
+    limit = 0
+    for f in range(len(minvalues)):
+        avmin = avmin + minvalues[f]
+    avmin /= len(minvalues)
+
+    def getmaxdif():
+        for g in range(len(minvalues)-1):
+            gaps.append(minvalues[g+1]-minvalues[g])
+        gaps.sort()
+        return gaps[-1]
+
+    def setnewmaxdif():
+        gaps.pop(-1)
+        return gaps[-1]
+    maxdif = getmaxdif()
+    while limit == 0:
+        for f in range(len(minvalues)-1):
+            if minvalues[f+1]-minvalues[f] == maxdif:
+                if minvalues[f+1]-minvalues[f] < avmin:
+                    limit = minvalues[f]
+                    return limit
+        maxdif = setnewmaxdif()
+
+
+def findrim(image, limitmod):
+    ignore = []
+    limit = getlimit(image) + limitmod
+    for a in range(len(image)):
+        if image[a].min() <= limit:
+            ignore.append(a)
+    return ignore
+
+
+def applyth(image):
+    newim = dict()
+    for th in range(len(image)):
+        newim[th] = mh.rc(image[th])
+    return newim
+
+
+def applygauss(image, intensity):
+    newim = dict()
+    for gau in range(len(image)):
+        newim[gau] = mh.gaussian_filter(image[gau], intensity)
+        newim[gau] = newim[gau].astype(np.uint8)
+    return newim
+
+
+def printprogress(iteration, total, prefix='', suffix='', decimals=2, barLength=100):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        barlength   - Optional  : character length of bar (Int)
+    """
+    formatStr = "{0:." + str(decimals) + "f}"
+    percent = formatStr.format(100 * (iteration / float(total)))
+    filledLength = int(round(barLength * iteration / float(total)))
+    bar = '█' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
+
+def measure(origimage, cuts, gaussintensity, iteration):
+    image = cut(origimage, cuts)
+    adjustsize(origimage, image, cuts)
+    if not default and iteration in modifyselection:
+        limitmod = modifyselection[iteration]
+    else:
+        limitmod = 0
+    ignore = findrim(image, limitmod)
+    avmod = len(ignore)
+    threshold = applyth(image)
+    if not default and iteration in increasegauss:
+        usedgaussintensity = gaussintensity ** increasegauss[iteration]
+    else:
+        usedgaussintensity = gaussintensity
+    gauss = applygauss(image, usedgaussintensity)
+    totalcellarea = 0.0
+    subcount = int(math.sqrt(len(image)))
+    for r in range(len(image)):
+        printprogress((iteration * len(image) + r + 1), (len(inputimages) * len(image)),
+                      prefix="processing " + str(len(inputimages)) + " images", suffix="complete")
+        if len(ignore) == 0 or r != ignore[0]:
+            cellarea = 0.0
+            area = image[r].shape[0] * image[r].shape[1]
+
+            gaussth = gauss[r] > threshold[r]
+
+            for array in gaussth:
+                for pixel in array:
+                    # variabel machen wenn Zellen hell
+                    if pixel == 0:
+                        cellarea += 1
+
+            cellperc = (cellarea / area)
+            inputimages[iteration][3] = cellperc * 100
+
+            rmin = mh.regmin(gaussth)
+            rmin = rmin.astype(np.uint8)
+
+            plt.subplot(subcount, subcount, r+1)
+            plt.axis('off')
+            plt.imshow(mh.overlay(image[r], rmin))
+
+            totalcellarea += cellperc
+        else:
+            plt.subplot(subcount, subcount, r+1)
+            plt.axis('off')
+            ignore.pop(0)
+
+    plt.title(str("ID: " + str(iteration) + ", day: " + str(inputimages[x][1]) + ", concentration: " + str(inputimages[x][2]) + ", coverage" + str(round((totalcellarea / ((len(image)) - avmod)) * 100, 2)) + " %"))
+    plt.axis('off')
+    pp.savefig()
+    plt.gcf().clear()
+
+for s in range(len(inputimages)):
+    measure(inputimages[s][0], 6, 2, s)
+
+print "creating pdf file..."
+
+
+def getgroups(inputdata):
+    groups = dict()
+    groups[0] = dict()
+    groups[0][0] = inputdata[0]
+    grcount = 0
+    grmemcount = 0
+    for c in range(len(inputdata)-1):
+        if groups[grcount][grmemcount][2] == inputdata[c+1][2]:
+            grmemcount += 1
+            groups[grcount][grmemcount] = inputdata[c+1]
+        else:
+            grcount += 1
+            grmemcount = 0
+            groups[grcount] = dict()
+            groups[grcount][grmemcount] = inputdata[c+1]
+    return groups
+
+
+def sumgroup(group):
+    singlelastadded = False
+    gsum = group[0][3]
+    daycount = 1
+    groupav = []
+    for gr in range(len(group)-1):
+        singlelastadded = False
+        if group[gr][1] == group[gr+1][1]:
+            gsum += group[gr+1][3]
+            daycount += 1
+        elif x == len(group)-1:
+            groupav.append(gsum/daycount)
+            groupav.append(group[gr+1][3])
+            singlelastadded = True
+        else:
+            groupav.append(gsum/daycount)
+            gsum = group[gr+1][3]
+            daycount = 1
+    if not singlelastadded:
+        groupav.append(gsum / daycount)
+    return groupav
+
+concentrationgroups = list()
+for x in range(len(inputimages)):
+    concentrationgroups.append(inputimages[x])
+concentrationgroups = sorted(concentrationgroups, key=itemgetter(2, 1))
+concentrationgroups = getgroups(concentrationgroups)
+congroupav = dict()
+for x in range(len(concentrationgroups)):
+    congroupav[x] = (sumgroup(concentrationgroups[x]))
+
+daysdata = []
+for x in range(len(inputimages)):
+    daysdata.append(inputimages[x][1])
+uniquedays = list(sorted(set(daysdata)))
+
+condata = []
+for x in range(len(inputimages)):
+    condata.append(float(inputimages[x][2]))
+uniquecons = list(sorted(set(condata)))
+
+conatday = dict()
+for x in range(len(congroupav[0])):
+    conatday[x] = list()
+    for y in range(len(congroupav)):
+        conatday[x].append(congroupav[y][x])
+
+for x in range(len(congroupav)):
+    plt.plot(uniquedays, list(congroupav[x]))
+
+pp.savefig()
+plt.gcf().clear()
+
+for x in range(len(conatday)):
+    plt.plot(uniquecons, list(conatday[x]))
+    pp.savefig()
+    plt.gcf().clear()
+
+pp.close()
---
+++ user_input.py
@@ -0,0 +1,50 @@
+"""
+This file can be used to modify the picture analysis.
+
+I recommend to first run the analysis with default settings, then look at the pdf.
+If some images seem wrong you can use this file to adjust the values for them.
+Then run the analysis again.
+
+Follow the instructions above each variable (the start with a "#") to modify the program.
+Never change anything but what is written after "="!
+
+If you enter values with fractional part make sure to use "." instead of ",".
+
+Don't forget to save the file before you run the analysis again.
+"""
+
+# Here you can increase the intensity of the gaussian filter for single images:
+# To add a values for one image just write "increasegauss[id] = value" without quotation marks.
+# Replace "id" with the image id displayed in the pdf and "value" with an integer of at least 2.
+# If you want to assign the same value to a row of images copy the following without quotation marks:
+"""
+firstid = replace this text with the id of the first image
+lastid = replace this text with the id of the last image
+newgauss = raplace this text with the gauss intensity you want to assign to the row of images
+for x in range(lastid-firstid):
+    increasegauss[x + firstid] = newgauss
+"""
+# You can add multiple rows of images if you want.
+increasegauss = dict()
+# Add all your changes under this line:
+
+
+# Here you can modify the value that is responsible for which image slices to ignore and which to keep.
+# If it seems that the algorithm ignored to many slices assign a negative value.
+# If it seems that the algorithm kept slices that should have been ignored assign a positive value.
+# I recommend to use values between -50 and 50
+# To add a values for one image just write "modifyselection[id] = value" without quotation marks.
+# Replace "id" with the image id displayed in the pdf and "value" with the value you want to assign.
+# Remember to use "." instead of "," when you use values with fractional part.
+# If you want to assign the same value to a row of images copy the following without quotation marks:
+"""
+firstid = replace this text with the id of the first image
+lastid = replace this text with the id of the last image
+modifyer = raplace this text with the value you want to assign to the row of images
+for x in range(lastid-firstid):
+    modifyselection[x + firstid] = modifyer
+"""
+# You can add multiple rows of images if you want.
+modifyselection = dict()
+# Add all your changes under this line:
+
